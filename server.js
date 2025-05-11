c525f561ae3a64b86919e5f0c5b85ea7d41ccfcf
nibin754 <nibinshan6@gmail.com>
2025-05-11 21:41:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 git add server.js
 git commit -m "Added server.js"
 git push origin main
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, Render!');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
